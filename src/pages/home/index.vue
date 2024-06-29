36fb4bc7311966f478bd74b1608005aa179f746a
lee <175937217@qq.com>
2024-06-29 17:54:19 +0800

    图标

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -3,10 +3,14 @@
     <!--      首页轮播图-->
     <Carousel/>
   </div>
+  <!--  首页搜索框-->
+  <Search/>
 </template>
 <script lang="ts" setup>
 //引入轮播图组件
 import Carousel from '@/pages/home/carousel/index.vue';
+//引入搜索组件
+import Search from '@/pages/home/search/index.vue';
 </script>
 <style lang="scss" scoped>
 
---
+++ src/pages/home/search/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="search">
+    <el-autocomplete
+        class="inline-input w-50"
+        clearable
+        placeholder="Please Input"
+    />
+    <el-button size="default" type="primary">搜索</el-button>
+  </div>
+</template>
+<script lang="ts" setup>
+
+</script>
+<style lang="scss" scoped>
+.search {
+  width: 100%;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 10px 0px;
+}
+</style>
