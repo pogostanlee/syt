380fbc9e8c82e7bb984a104beacc2794cfbb6754
lee <175937217@qq.com>
2024-07-02 00:07:08 +0800

    等级组件

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,21 +1,27 @@
 <template>
   <div>
     <!--      首页轮播图-->
-    <Carousel/>
+    <Carousel />
   </div>
   <!--  首页搜索框-->
-  <Search/>
+  <Search />
   <!-- 展示医院页面-->
   <el-row :gutter="20">
-    <el-col :span="12">123</el-col>
+    <el-col :span="12">
+      <!--      等级组件-->
+      <Level></Level>
+      <!--      地区组件-->
+    </el-col>
     <el-col :span="12">444</el-col>
   </el-row>
 </template>
 <script lang="ts" setup>
-//引入轮播图组件
-import Carousel from '@/pages/home/carousel/index.vue';
-//引入搜索组件
-import Search from '@/pages/home/search/index.vue';
+  //引入轮播图组件
+  import Carousel from '@/pages/home/carousel/index.vue'
+  //引入搜索组件
+  import Search from '@/pages/home/search/index.vue'
+  //引入首页等级的组件
+  import Level from '@/pages/home/level/index.vue'
 </script>
 <style lang="scss" scoped>
 
---
+++ src/pages/home/level/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="level">
+    <h1>医院</h1>
+    <div class="content">
+      <div class="left">等级：
+        <ul class="hospital">
+          <li class="active">去1111</li>
+          <li>去</li>
+          <li>去</li>
+          <li>去</li>
+          <li>去</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+
+</script>
+<style lang="scss" scoped>
+  .level {
+    color: #7f7f7f;
+    
+    h1 {
+      font-weight: 900;
+      margin: 10px 0;
+    }
+    
+    .content {
+      display: flex;
+      
+      .left {
+        margin-right: 10px;
+      }
+      
+      .hospital {
+        display: flex;
+        
+        li {
+          margin-right: 10px;
+          
+          &.active {
+            color: #55a6fe;
+          }
+        }
+        
+        li:hover {
+          color: #55a6fe;
+          cursor: pointer;
+        }
+      }
+    }
+  }
+</style>
